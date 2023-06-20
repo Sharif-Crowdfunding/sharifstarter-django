926f400b0d9db2a7ce1afbe844c6bbf1d69fa038
Hosein Ahangari <hoseinahangari328@gmail.com>
2023-06-20 20:11:18 +0330

    update all ganache url.

--- utils/contract/contract.py
+++ utils/contract/contract.py
@@ -2,7 +2,7 @@ import json
 
 from web3 import Web3
 
-ganache_url = 'http://193.151.153.56:8545'
+ganache_url = 'http://127.0.0.1:8545'
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 print(web3.isConnected())
